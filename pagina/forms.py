602a05f0c54d5c140f9db8ecffffa29b45576b58
Ricardo <xdricardo63@gmail.com>
2023-05-30 16:00:02 -0600

    idiomas

--- pagina/forms.py
+++ pagina/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
-
-
 class TextoEs(forms.Form):
-    texto = forms.CharField(widget=forms.Textarea(attrs={'class':'flex p-2.5 w-full text-2xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500', 'placeholder': 'Introduce el texto' ,'rows':'6'}))
+    texto = forms.CharField(widget=forms.Textarea(attrs={'class':'flex p-2.5 w-full text-2xl text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-Quicksand', 'placeholder': 'Introduce el texto' ,'rows':'6'}))
 
+class InputS(forms.Form):
+    opciones = (
+        ('in', 'Ingles'),
+        ('fr', 'Frances'),
+        ('de', 'Aleman'),
+        ('tr', 'Turco'),
+    )
+    idioma = forms.ChoiceField(choices=opciones, widget=forms.Select(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-Quicksand'}))
--- pagina/views.py
+++ pagina/views.py
@@ -1,43 +1,50 @@
 from django.shortcuts import render
-from .forms import TextoEs
+from .forms import TextoEs, InputS
 from mtranslate import translate
 import pyttsx3
 
-def traducir_y_reproducir_voz(oracion):
-    traduccion = translate(oracion, 'en')
 
+def traducir(oracion, lenguaje):
+    traduccion = translate(oracion, lenguaje)
     # Inicializar el motor de síntesis de voz
     engine = pyttsx3.init()
     engine.setProperty('rate', 150)  # Velocidad de reproducción (opcional)
-
     # Reproducir la traducción como voz
     engine.say(traduccion)
     engine.runAndWait()
-
     return traduccion
-
-def traducir(oracion):
-   traduccion = translate(oracion, 'en')
-   return traduccion
 
 def index(request):
     texto_a_traducir = request.GET.get('texto', '')
-    if texto_a_traducir:
-        resultado = traducir(texto_a_traducir)
-        print(resultado)
-        resultado = traducir_y_reproducir_voz(texto_a_traducir)
+    idioma = request.GET.get('idioma', 'in')  # Obtener el valor del idioma seleccionado, por defecto es 'Español'
+    resultado = ''
+    lblOrigen = 'Español'
+    lblDestino = 'Inglés'
+    idioma_codigo = {
+    'in': 'in',
+    'de': 'de',
+    'tr': 'tr',
+    }
+
+    if texto_a_traducir and idioma in idioma_codigo:
+         resultado = traducir(texto_a_traducir, idioma_codigo[idioma])
     else:
-        resultado = ''
+        if texto_a_traducir:
+            resultado = traducir(texto_a_traducir, 'fr')
+
+
 
     print(resultado)
+    print(idioma)
+
     return render(request, 'index.html', {
         'TextoEs' : TextoEs(initial={'texto': texto_a_traducir}),
-        'resultado': resultado              
+        'InputS' : InputS(initial={'idioma': idioma}),
+        'resultado': resultado,
+        'lblOrigen': lblOrigen,
+        'lblDestino': lblDestino
     })
 
 def integrantes(request):
     return render(request, 'integrantes.html')
-
-def traductor(request):
-    return render(request, 'traductor.html')
 
